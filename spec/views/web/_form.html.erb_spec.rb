7f942456cf56b0be809b01ba9c6eb8f73817da12
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-01 23:41:08 +0300

    Add spec for a present url in the form

--- spec/views/web/_form.html.erb_spec.rb
+++ spec/views/web/_form.html.erb_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe 'web/_form.html.erb', type: :view do
     render
     expect(rendered).to match(/name="commit"/)
   end
+
+  context 'with the url in params' do
+    it 'displays this url' do
+      url = 'http://getskeleton.com/'
+      assign(:url, url)
+      render
+      expect(rendered).to match(url)
+    end
+  end
 end
