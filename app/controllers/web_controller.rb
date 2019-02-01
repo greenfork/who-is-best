3a540f4d2830d361ff56486c5a634679daccd77d
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-01 23:38:34 +0300

    Add stub for url in url field

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -6,5 +6,6 @@ class WebController < ApplicationController
     @contributors = [{ name: 'me', url: 'url' },
                      { name: 'mi', url: 'urt' },
                      { name: 'mo', url: 'uri' }]
+    @url = 'https://github.com'
   end
 end
