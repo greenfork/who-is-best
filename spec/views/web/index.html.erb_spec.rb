55a73644ace214e421b267bc31114389ee6bd6b7
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-01 23:16:14 +0300

    Add spec for web#index

--- spec/views/web/index.html.erb_spec.rb
+++ spec/views/web/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "web/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'web/index.html.erb', type: :view do
+  it 'has a form' do
+    render
+    expect(rendered).to match(/<form/)
+  end
 end
