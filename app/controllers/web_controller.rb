fceb4b2ad273b30c071582730512448cba54c6ae
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-02 20:49:57 +0300

    Change data accesses for web#show view

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -3,9 +3,15 @@ class WebController < ApplicationController
   end
 
   def show
-    @contributors = [{ name: 'me', url: 'url' },
-                     { name: 'mi', url: 'urt' },
-                     { name: 'mo', url: 'uri' }]
     @url = 'https://github.com'
+    contribs_stub
+  end
+
+  private
+
+  def contribs_stub
+    @contributors = [{ name: 'me', filename: 'me.pdf' },
+                     { name: 'mi', filename: 'mi.pdf' },
+                     { name: 'mo', filename: 'mo.pdf' }]
   end
 end
