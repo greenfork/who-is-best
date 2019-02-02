32daae3f1a0ea9db9d3c1b97de5aaf0dac659885
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-02 13:56:28 +0300

    Add spec for Api::Github::Rest

---
+++ spec/libs/api/github/rest_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Api::Github::Rest do
+  let(:contrib_url) { "https://api.github.com/repos/#{repo}/contributors" }
+
+  context 'with valid repository address' do
+    let(:repo) { 'ruby/ruby' }
+    let(:get_response) { file_fixture('rest_response.json').read }
+    let(:contribs) { %w[rafaelfranca tenderlove dhh] }
+
+    context 'and with OAuth token' do
+      let(:oauth_token) { 'qwertyuioasdfgh' }
+      let(:headers) do
+        { accept: 'application/vnd.github.v3+json',
+          authorization: "token #{oauth_token}" }
+      end
+
+      it 'returns at most 3 names of most active contributors' do
+        client = class_double('RestClient', get: get_response)
+        rs = described_class.new(repo, oauth_token, client).contributors(3)
+        expect(rs).to eq(contribs)
+      end
+
+      it 'sends correct params to RestClient gem' do
+        client = class_double('RestClient', get: '[{"login": "me"}]')
+        expect(client).to receive(:get).with(contrib_url, headers)
+        described_class.new(repo, oauth_token, client).contributors(3)
+      end
+    end
+
+    context 'and with no OAuth token' do
+      let(:oauth_token) { nil }
+      let(:headers) { { accept: 'application/vnd.github.v3+json' } }
+
+      it 'returns at most 3 names of most active contributors' do
+        client = class_double('RestClient', get: get_response)
+        rs = described_class.new(repo, oauth_token, client).contributors(3)
+        expect(rs).to eq(contribs)
+      end
+
+      it 'sends correct params to RestClient gem' do
+        client = class_double('RestClient', get: '[{"login": "me"}]')
+        expect(client).to receive(:get).with(contrib_url, headers)
+        described_class.new(repo, oauth_token, client).contributors(3)
+      end
+    end
+  end
+end
