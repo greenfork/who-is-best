d574248ef982d1eb71a331cf9d86c86c5746557a
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-02 20:53:16 +0300

    Create new route for file downloads

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,16 +1,43 @@
 class WebController < ApplicationController
+  class InvalidFilename < RuntimeError; end
+  TMP_DIR = '/tmp'.freeze
+
   def index
   end
 
   def show
     contribs_stub
     @url = url_params[:repository]
+    @contributors.each_with_index do |hash, index|
+      Pdf::Certificate.generate(hash[:name], index + 1,
+                                filename: File.join(TMP_DIR, hash[:filename]))
+    end
+  end
+
+  def download
+    filename = download_params
+  rescue InvalidFilename
+    redirect_to web_show_path
+  else
+    send_file filename, type: 'application/pdf'
   end
 
   private
 
   def url_params
     params.permit(:repository)
+  end
+
+  def download_params
+    if params[:filename].blank? || params[:format] != 'pdf'
+      raise InvalidFilename
+    end
+
+    filename = params[:filename] + '.' + params[:format]
+    path = File.join(TMP_DIR, filename)
+    raise InvalidFilename unless File.exist?(path)
+
+    path
   end
 
   def contribs_stub
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'web#index'
   get 'web/show'
+  get '/download/:filename', to: 'web#download', as: :download
 end
