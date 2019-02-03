b95405d19797cfb5eb921e71d9e30ef80d816680
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-03 12:08:39 +0300

    Fix failing specs

--- spec/views/web/show.html.erb_spec.rb
+++ spec/views/web/show.html.erb_spec.rb
@@ -8,19 +8,17 @@ RSpec.describe 'web/show.html.erb', type: :view do
 
   context 'with the contributors present' do
     let(:names) { %w[me mi mo] }
-    let(:filenames)  { %w[filename1 filename2 filename3] }
 
     before(:context) do
-      assign(:contributors, [{ name: 'me', filename: 'filename1' },
-                             { name: 'mi', filename: 'filename2' },
-                             { name: 'mo', filename: 'filename3' }])
+      assign(:contributors, [{ name: 'me' },
+                             { name: 'mi' },
+                             { name: 'mo' }])
     end
 
     it 'has a name and a link for every contributor' do
       render
-      names.zip(filenames).each do |name, filename|
+      names.each do |name|
         expect(rendered).to match(name)
-        expect(rendered).to match(filename)
       end
     end
 
